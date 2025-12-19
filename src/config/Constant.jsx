995633028d569935725189c9304e6e78b35d87d2
Rj2398 <yesitlabs.rajanmalakar@gmail.com>
2025-12-19 14:37:41 +0530

    C

--- src/config/Constant.jsx
+++ src/config/Constant.jsx
@@ -1,5 +1,5 @@
-const BaseUrl = "https://awplconnectadmin.tgastaging.com";
-// const BaseUrl = "https://admin.awplconnect.com";
+// const BaseUrl = "https://awplconnectadmin.tgastaging.com";
+const BaseUrl = "https://admin.awplconnect.com";
 
 export default {
   BaseUrl,
